171c17f02e853be894ae551363e4f8731d27185c
Khushter Nizame <66989627+Khushter420@users.noreply.github.com>
2021-06-26 11:39:44 +0530

    Create Premium_user.js

---
+++ controller/Premium_user.js
@@ -0,0 +1,47 @@
+const Razorpay = require('razorpay');
+const Order = require('../models/premium')
+
+
+exports.purchasepremium =async (req, res) => {
+    try {
+        var rzp = new Razorpay({
+            key_id:rzp_test_LznwdL1dJNxm4Z,
+            key_secret:pMaA7mVTqbMP8VV5eBfcxCQK
+        })
+        const amount = 5000;
+
+        rzp.orders.create({amount, currency: "INR"}, (err, order) => {
+            if(err) {
+                throw new Error(err);
+            }
+            req.user.createOrder({ orderid: order.id, status: 'PENDING'}).then(() => {
+                return res.status(201).json({ order, key_id : rzp.key_id});
+
+            }).catch(err => {
+                throw new Error(err)
+            })
+        })
+    } catch(err){
+        console.log(err);
+        res.status(403).json({ message: 'Sometghing went wrong', error: err})
+    }
+}
+
+exports.updateTransactionStatus = (req, res ) => {
+    try {
+        const { payment_id, order_id} = req.body;
+        Order.findOne({where : {orderid : order_id}}).then(order => {
+            order.update({ paymentid: payment_id, status: 'SUCCESSFUL'}).then(() => {
+                return res.status(202).json({sucess: true, message: "Transaction Successful"});
+            }).catch((err)=> {
+                throw new Error(err);
+            })
+        }).catch(err => {
+            throw new Error(err);
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(403).json({ errpr: err, message: 'Sometghing went wrong' })
+
+    }
+}
